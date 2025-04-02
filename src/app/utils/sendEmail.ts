33aa0a6ab4918003dea350a98bcb7c31abaf2a43
Enamul-Haque-Shojib <enamulhaqueshojib@gmail.com>
2025-04-02 20:01:12 +0600

    update approve booking

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -3,22 +3,31 @@ import config from '../config';
 
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
+  
+  try {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com.',
+    host: 'smtp.gmail.com',
     port: 587,
     secure: config.NODE_ENV === 'production',
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: 'eshenamulhaque@gmail.com',
-      pass: 'crxf aeup hxrx knwv',
+      user: config.email_user,
+      pass: config.email_pass,
     },
   });
 
   await transporter.sendMail({
-    from: 'eshenamulhaque@gmail.com', // sender address
+    from: config.email_user, // sender address
     to, // list of receivers
     subject, // Subject line
     text: '', // plain text body
     html, // html body
   });
+
+} catch (error) {
+  console.error('❌ Error sending email:', error);
+  throw new Error('Email sending failed'); // Ensures the server doesn’t crash
+}
+
+
 };
